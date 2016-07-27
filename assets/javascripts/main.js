1387881ee762a3921a2971384e52eb75fd9110bd
Dmitri <orldm@mail.ru>
2016-07-27 12:33:15 +0300

    adjust media queries

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 35 && $(window).width() > 753) {
+        if ($(window).scrollTop() > 35 && $(window).width() > 752) {
             $('.navbar-wrapper').addClass('navbar-fixed');
-            var alpha =  Math.min(0.75 ,($(window).scrollTop() - 35)/135);
+            var alpha =  Math.min(0.95 ,($(window).scrollTop() - 35)/135);
             var fontColor = parseInt(255 * (1 - Math.min(1.0, ($(window).scrollTop() - 35)/135)), 10);
             $('.navbar-wrapper').css('background-color', 'rgba(255, 255, 255, '+alpha+')');
             $('.js-anchor').css('color', 'rgb('+fontColor+','+fontColor+','+fontColor+')');
             $('.js-anchor').addClass('nav-link-fixed');
             $('.logo-placeholder').css('display', 'none');
         }
-        if ($(window).scrollTop() < 35 && $(window).width() > 753) {
+        if ($(window).scrollTop() < 35 && $(window).width() > 752) {
             $('.navbar-wrapper').removeClass('navbar-fixed');
             $('.navbar-wrapper').css('background-color', 'rgba(255, 255, 255, 0)');
             $('.js-anchor').removeClass('nav-link-fixed');
@@ -18,12 +18,12 @@ $(document).ready(function() {
         }
     });
     $(window).resize(function() {
-        if ($(window).scrollTop() > 35 && $(window).width() > 753) {
+        if ($(window).scrollTop() > 35 && $(window).width() > 752) {
             $('.navbar-wrapper').addClass('navbar-fixed');
             $('.js-anchor').addClass('nav-link-fixed');
             $('.js-anchor').css('color','black');
             $('.logo-placeholder').css('display', 'none');
-        } else if ($(window).scrollTop() > 35 && $(window).width() < 753) {
+        } else if ($(window).scrollTop() > 35 && $(window).width() < 752) {
             $('.navbar-wrapper').removeClass('navbar-fixed');
             $('.js-anchor').removeClass('nav-link-fixed');
             $('.js-anchor').css('color','white');
